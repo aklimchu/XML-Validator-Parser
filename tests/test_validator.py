af6802029ed74cc84ccfff41c069d6e1775e6865
aklimchu <berkyt00@gmail.com>
2025-12-14 16:51:01 +0200

    tests(new tests, example XMLs): Add new tests, valid and invalid XMLs to use as examples

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,5 +1,20 @@
 import pytest
 from xml_validator import validate_and_parse
 
-def test_valid_xml_passes_validation():
-    validate_and_parse()
+VALID_XML = "tests/valid_example.xml"
+INVALID_XML = "tests/invalid_example.xml"
+XSD = "schemas/pain.001.001.09.xsd"
+
+def test_valid_xml_passes_validation(capsys):
+    validate_and_parse(VALID_XML, XSD)
+    captured = capsys.readouterr()
+    assert "✅ XML is valid" in captured.out
+
+def test_invalid_xml_fails_validation(capsys):
+    validate_and_parse(INVALID_XML, XSD)
+    captured = capsys.readouterr()
+    assert "❌ XML is INVALID" in captured.out
+
+def test_parses_key_fields():
+    # Can be added later
+    pass
